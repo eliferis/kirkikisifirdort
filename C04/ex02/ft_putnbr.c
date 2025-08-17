8ca2193246d8a32d6933b571da2875ad379d9de1
Elif Eris <elifbirdir@gmail.com>
2025-08-17 08:59:35 +0200

    Update ft_putnbr.c

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -1,36 +1,26 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: apuchill <apuchill@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/12/05 11:22:03 by apuchill          #+#    #+#             */
-/*   Updated: 2019/12/05 11:22:10 by apuchill         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+
 
 #include <unistd.h>
 
-void	ft_putchar(char a)
+void	ft_putchar(char c)
 {
-	write(1, &a, 1);
+	write(1, &c, 1);
 }
 
 void	ft_putnbr(int nb)
 {
-	if (nb == -2147483648)
+	if (nb == -2147483648) //integer minimum = -2147483648
 	{
 		ft_putchar('-');
 		ft_putchar('2');
-		nb = 147483648;
+		nb = 147483648; / Kalanını yazmak için sayıyı değiştir
 	}
-	if (nb < 0)
+	if (nb < 0) //sign changing
 	{
 		ft_putchar('-');
 		nb *= -1;
 	}
-	if (nb < 10)
+	if (nb < 10) //'0' chars ascii value is equal to 48. we turn the number to char. Sayı tek basamaklıysa (0-9) direkt karaktere cevirilip yazilir.
 	{
 		ft_putchar(nb + 48);
 		return ;
@@ -38,4 +28,20 @@ void	ft_putnbr(int nb)
 	else
 		ft_putnbr(nb / 10);
 	ft_putnbr(nb % 10);
+}
+
+
+
+int main(void)
+{
+	ft_putnbr(42);         // 42
+	write(1, "\n", 1);
+	ft_putnbr(-123);       // -123
+	write(1, "\n", 1);
+	ft_putnbr(0);          // 0
+	write(1, "\n", 1);
+	ft_putnbr(-2147483648); // -2147483648
+	write(1, "\n", 1);
+	ft_putnbr(2147483647);  // 2147483647
+	write(1, "\n", 1);
 }
