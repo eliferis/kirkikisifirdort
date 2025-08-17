2d486e717509eee303f1a3d3cf2dac2732f1070b
Elif Eris <elifbirdir@gmail.com>
2025-08-17 09:15:40 +0200

    Update ft_atoi.c

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -1,36 +1,26 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: apuchill <apuchill@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/12/05 11:21:46 by apuchill          #+#    #+#             */
-/*   Updated: 2019/12/05 19:58:52 by apuchill         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+
 
 int		ft_atoi(char *str)
 {
-	int c;
-	int s;
-	int res;
+	int c; //karakterleri sirayla okumak icin
+	int sign; 
+	int result; 
 
 	c = 0;
-	s = 1;
-	res = 0;
-	while ((str[c] >= '\t' && str[c] <= '\r') || str[c] == ' ')
+	sign = 1;
+	result = 0;
+	while ((str[c] >= '\t' && str[c] <= '\r') || str[c] == ' ') //skip the spaces
 		c++;
-	while (str[c] == '+' || str[c] == '-')
+	while (str[c] == '+' || str[c] == '-') //check the sign
 	{
 		if (str[c] == '-')
-			s *= -1;
+			sign *= -1;
 		c++;
 	}
-	while (str[c] >= '0' && str[c] <= '9')
+	while (str[c] >= '0' && str[c] <= '9') //rakamlari sirayla okuyor ve sayiya ceviriyor
 	{
-		res = (str[c] - '0') + (res * 10);
+		result = (str[c] - '0') + (result * 10);
 		c++;
 	}
-	return (res * s);
+	return (result * sign);
 }
